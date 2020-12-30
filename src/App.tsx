2fb0e89ca125f3b9fccbdd697e67e2df0195e409
ryosuketter <r.shimizu.yale@gmail.com>
2020-12-30 22:35:14 +0900

    不要になった初期データの削除

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
 import React from 'react'
-import logo from './logo.svg'
-import './App.css'
 
 const App = () => {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>テキスト変更</p>
-				<a className='App-link' href='https://reactjs.org' target='_blank' rel='noopener noreferrer'>
-					Learn React
-				</a>
-			</header>
-		</div>
-	)
+	return <p>テスト</p>
 }
 
 export default App
