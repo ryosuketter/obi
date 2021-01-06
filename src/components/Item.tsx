4702276c680e4e16d47a62fa4b1cf426d6a4d4ad
ryosuketter <r.shimizu.yale@gmail.com>
2021-01-07 06:52:44 +0900

    React modal added

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from 'react-modal'
+
+Modal.setAppElement('#root')
+
+const customStyles: Modal.Styles = {
+	content: {
+		top: '50%',
+		left: '50%',
+		right: 'auto',
+		bottom: 'auto',
+		marginRight: '-50%',
+		transform: 'translate(-50%, -50%)'
+	}
+}
 
 type Props = {
 	book: any
 }
 
 const Item: React.FC<Props> = ({ book }) => {
+	const [modalIsOpen, setIsOpen] = useState(false)
+
+	const openModal = (): void => setIsOpen(true)
+	const closeModal = (): void => setIsOpen(false)
+
 	const {
 		volumeInfo: { title, imageLinks }
 	} = book
@@ -18,8 +37,13 @@ const Item: React.FC<Props> = ({ book }) => {
 			{imageLinks && (
 				<div>
 					<img src={imageLinks.smallThumbnail} alt={title}></img>
+					<button onClick={openModal}>モーダル表示</button>
 				</div>
 			)}
+			<Modal id='root' isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles}>
+				<button onClick={closeModal}>close</button>
+				<img src={imageLinks.smallThumbnail} alt={title}></img>
+			</Modal>
 		</li>
 	)
 }
