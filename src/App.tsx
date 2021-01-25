4ac79fd338e9132e942f393bc073136a49ef10f1
ryosuketter <r.shimizu.yale@gmail.com>
2021-01-26 01:02:52 +0900

    interface BookTypes moved module file and export

--- src/App.tsx
+++ src/App.tsx
@@ -1,76 +1,7 @@
 import React, { useState, useCallback } from 'react'
 import List from './components/List'
 import SearchFrom from './components/SearchFrom'
-
-interface BookTypes {
-  accessInfo: {
-    accessViewStatus: string
-    country: string
-    viewability: string
-    embeddable: boolean
-    publicDomain: boolean
-    textToSpeechPermission: string
-    epub: {
-      isAvailable: boolean
-      downloadLink: string
-      acsTokenLink: string
-    }
-    pdf: {
-      isAvailable: boolean
-      downloadLink: string
-      acsTokenLink: string
-    }
-    webReaderLink: string
-    downloadAccess?: {
-      kind: 'books#downloadAccessRestriction'
-      volumeId: string
-      restricted: boolean
-      deviceAllowed: boolean
-      justAcquired: boolean
-      maxDownloadDevices: number
-      downloadsAcquired: number
-      nonce: string
-      source: string
-      reasonCode: string
-      message: string
-      signature: string
-    }
-  }
-  etag: string
-  id: string
-  kind: 'books#volume'
-  saleInfo: {
-    country: string
-    saleability: string
-    onSaleDate?: string
-    isEbook: boolean
-    listPrice?: {
-      amount: number
-      currencyCode: string
-    }
-    retailPrice?: {
-      amount: number
-      currencyCode: string
-    }
-    buyLink?: string
-  }
-  searchInfo: {
-    textSnippet: string
-  }
-  selfLink: string
-  volumeInfo: {
-    title: string
-    categories?: string[]
-    publisher?: string
-    authors: string[]
-    description: string
-    infoLink: string
-    imageLinks?: {
-      thumbnail: string
-    }
-    publishedDate: string
-  }
-}
+import { BookTypes } from './Types'
 
 const App: React.FC = () => {
   const [books, setBooks] = useState<BookTypes[]>([])
--- src/Types.ts
+++ src/Types.ts
@@ -1,3 +1,73 @@
 export type Book = {
   id: string
 }
+
+export interface BookTypes {
+  accessInfo: {
+    accessViewStatus: string
+    country: string
+    viewability: string
+    embeddable: boolean
+    publicDomain: boolean
+    textToSpeechPermission: string
+    epub: {
+      isAvailable: boolean
+      downloadLink: string
+      acsTokenLink: string
+    }
+    pdf: {
+      isAvailable: boolean
+      downloadLink: string
+      acsTokenLink: string
+    }
+    webReaderLink: string
+    downloadAccess?: {
+      kind: 'books#downloadAccessRestriction'
+      volumeId: string
+      restricted: boolean
+      deviceAllowed: boolean
+      justAcquired: boolean
+      maxDownloadDevices: number
+      downloadsAcquired: number
+      nonce: string
+      source: string
+      reasonCode: string
+      message: string
+      signature: string
+    }
+  }
+  etag: string
+  id: string
+  kind: 'books#volume'
+  saleInfo: {
+    country: string
+    saleability: string
+    onSaleDate?: string
+    isEbook: boolean
+    listPrice?: {
+      amount: number
+      currencyCode: string
+    }
+    retailPrice?: {
+      amount: number
+      currencyCode: string
+    }
+    buyLink?: string
+  }
+  searchInfo: {
+    textSnippet: string
+  }
+  selfLink: string
+  volumeInfo: {
+    title: string
+    categories?: string[]
+    publisher?: string
+    authors: string[]
+    description: string
+    infoLink: string
+    imageLinks?: {
+      thumbnail: string
+    }
+    publishedDate: string
+  }
+}
