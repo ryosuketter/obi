953e73713267c1348e96f621a64b98ecc520755d
ryosuketter <r.shimizu.yale@gmail.com>
2021-06-27 21:49:25 +0900

    必要な処理を必要なコンポネントに

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,14 @@
-import React, { useState, useCallback } from 'react'
-import List from './components/List'
-import SearchFrom from './components/SearchFrom'
+import React, { useState } from 'react'
+import { List } from './components/List'
+import { SearchFrom } from './components/SearchFrom'
 import { BookTypes } from './Types'
 
 const App: React.FC = () => {
   const [books, setBooks] = useState<BookTypes[]>([])
-  const [searchString, setSearchString] = useState('')
-
-  const handleClick: (event: React.ChangeEvent<HTMLInputElement>) => void = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setSearchString(event.target.value)
-    },
-    [setSearchString]
-  )
-
-  const setSearchResult: (state: []) => void = useCallback(
-    state => {
-      setBooks(state)
-    },
-    [setSearchString]
-  )
 
   return (
     <>
-      <SearchFrom handleClick={handleClick} setSearchResult={setSearchResult} searchString={searchString} />
+      <SearchFrom setBooks={setBooks} />
       <List books={books} />
     </>
   )
